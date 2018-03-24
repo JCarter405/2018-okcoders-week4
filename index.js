70858a28dd6ce37ce3aba655863ac6267c4ebc05
Justin Carter <caufield365@yahoo.com>
2018-03-24 11:31:51 -0500

    add additional json

---
+++ index.js
@@ -0,0 +1,10 @@
+var $ = require("jquery");
+var _ = require("lodash");
+
+
+var list = [ "john", "justin", "kelly", "nathan", "leo"];
+
+function message(){
+	console.log("this is a message");
+	alert("this is a message");
+}
